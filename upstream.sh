a0c0eccdeb5abefa571e82fff5a48a8789fef8a3
fernvenue <git@fernvenue.com>
2023-10-05 05:59:37 +0000

    Change IPv4 IPv6 behavior.

--- upstream.sh
+++ upstream.sh
@@ -2,7 +2,7 @@
 set -e
 DATE=`date --rfc-3339 sec`
 echo "$DATE: IPv4 connection testing..."
-if ping -c 3 "223.5.5.5" > /dev/null 2>&1; then
+if ping -c 3 "223.6.6.6" > /dev/null 2>&1; then
 	IPv4="true"
 fi
 echo "$DATE: IPv6 connection testing..."
@@ -30,7 +30,11 @@ echo "$DATE: Getting data updates..."
 curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/var/tmp/chinalist.upstream"
 echo "$DATE: Processing data format..."
 cat "/var/tmp/default.upstream" "/var/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
-if ! [[ $IPv4 == "true" ]]; then sed -i "s|114.114.114.114|2001:da8::666|g" /usr/share/adguardhome.upstream; fi
+if [[ $IPv4 == "true" ]]; then
+	sed -i "s|114.114.114.114|h3://223.6.6.6:443/dns-query|g" /usr/share/adguardhome.upstream
+else
+	sed -i "s|114.114.114.114|2400:3200::1|g" /usr/share/adguardhome.upstream
+fi
 echo "$DATE: Cleaning..."
 rm /var/tmp/*.upstream
 systemctl restart AdGuardHome
