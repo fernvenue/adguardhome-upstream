6e6ebc48487e07ea6f22d9b1c1f152b6a8e830dc
sumuen <zim29144@gmail.com>
2023-11-16 23:34:28 +0800

    adjust upstream.sh to use the new one_step.sh script · 1b0b6b6d

---
+++ one_step.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 检查并创建 /usr/local 目录
+if [ ! -d "/usr/local" ]; then
+    mkdir -p /usr/local
+    echo "已创建 /usr/local/ 目录。"
+fi
+# 下载 upstream.sh 脚本并赋予执行权限
+curl -o "/usr/local/bin/upstream.sh" "https://github.com/sumuen/adguardhome-upstream/blob/master/upstream.sh"
+chmod +x /usr/local/bin/upstream.sh
+# 运行 upstream.sh 脚本
+/usr/local/bin/upstream.sh
--- upstream.sh
+++ upstream.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 set -e
-DATE=`date --rfc-3339 sec`
+DATE=$(date -Iseconds)
+echo "$DATE: IPv4 connection testing..."
+DATE=$(date -Iseconds)
 echo "$DATE: IPv4 connection testing..."
 if ping -c 3 "223.6.6.6" > /dev/null 2>&1; then
-	IPv4="true"
+        IPv4="true"
 fi
 echo "$DATE: IPv6 connection testing..."
 if ping -c 3 "2400:3200:baba::1" > /dev/null 2>&1; then
-	IPv6="true"
+        IPv6="true"
 fi
 if [[ $IPv4 == "true" ]]; then
-	if [[ $IPv6 == "true" ]]; then
-		echo "$DATE: IPv4 and IPv6 connections both available."
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
-	else
-		echo "$DATE: IPv4 connection available."
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
-	fi
+        if [[ $IPv6 == "true" ]]; then
+                echo "$DATE: IPv4 and IPv6 connections both available."
+                curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
+        else
+                echo "$DATE: IPv4 connection available."
+                curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
+        fi
 else
-	if [[ $IPv6 == "true" ]]; then
-		echo "$DATE: IPv6 connection available."
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
-	else
-		echo "ERROR: No available network connection was detected, please try again."
-		exit 1
-	fi
+        if [[ $IPv6 == "true" ]]; then
+                echo "$DATE: IPv6 connection available."
+                curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
+        else
+                echo "ERROR: No available network connection was detected, please try again."
+                exit 1
+        fi
 fi
 echo "$DATE: Getting data updates..."
 curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/var/tmp/chinalist.upstream"
 echo "$DATE: Processing data format..."
-cat "/var/tmp/default.upstream" "/var/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
+cat "/var/tmp/default.upstream" "/var/tmp/chinalist.upstream" > /usr/local/share/adguardhome.upstream
 if [[ $IPv4 == "true" ]]; then
-	sed -i "s|114.114.114.114|h3://223.5.5.5:443/dns-query h3://223.6.6.6:443/dns-query|g" /usr/share/adguardhome.upstream
+        sed -i "s|114.114.114.114|h3://223.5.5.5:443/dns-query h3://223.6.6.6:443/dns-query|g" /usr/local/share/adguardhome.upstream
 else
-	sed -i "s|114.114.114.114|2400:3200::1 2400:3200:baba::1|g" /usr/share/adguardhome.upstream
+        sed -i "s|114.114.114.114|2400:3200::1 2400:3200:baba::1|g" /usr/local/share/adguardhome.upstream
 fi
 echo "$DATE: Cleaning..."
 rm /var/tmp/*.upstream
-systemctl restart AdGuardHome
+/etc/init.d/AdGuardHome restart
 echo "$DATE: All finished!"
