3b125a770320ddc8587f87ada8b79093e404aa8c
fernvenue <git@fernvenue.com>
2023-11-17 01:41:39 +0800

    Change tmp directory for better compatibility.

--- upstream.sh
+++ upstream.sh
@@ -29,7 +29,7 @@ if [[ $IPv4 == "true" ]]; then
 			DATE=`date --rfc-3339 sec`
 			echo "$DATE: IPv4 and IPv6 connections both available."
 		fi
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
+		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
 	else
 		if [[ $SYSTEMD == 1 ]]; then
 			echo "Only IPv4 connection available."
@@ -37,7 +37,7 @@ if [[ $IPv4 == "true" ]]; then
 			DATE=`date --rfc-3339 sec`
 			echo "$DATE: Only IPv4 connection available."
 		fi
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
+		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
 	fi
 else
 	if [[ $IPv6 == "true" ]]; then
@@ -47,7 +47,7 @@ else
 			DATE=`date --rfc-3339 sec`
 			echo "$DATE: Only IPv6 connection available."
 		fi
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
+		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
 	else
 		if [[ $SYSTEMD == 1 ]]; then
 			echo "No available network connection was detected, please try again."
@@ -64,14 +64,14 @@ else
 	DATE=`date --rfc-3339 sec`
     echo "$DATE: Getting data updates..."
 fi
-curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/var/tmp/chinalist.upstream"
+curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/tmp/chinalist.upstream"
 if [[ $SYSTEMD == 1 ]]; then
     echo "Processing data format..."
 else
 	DATE=`date --rfc-3339 sec`
     echo "$DATE: Processing data format..."
 fi
-cat "/var/tmp/default.upstream" "/var/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
+cat "/tmp/default.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
 if [[ $IPv4 == "true" ]]; then
 	sed -i "s|114.114.114.114|h3://223.5.5.5:443/dns-query h3://223.6.6.6:443/dns-query|g" /usr/share/adguardhome.upstream
 else
@@ -83,7 +83,7 @@ else
 	DATE=`date --rfc-3339 sec`
     echo "$DATE: Cleaning..."
 fi
-rm /var/tmp/*.upstream
+rm /tmp/*.upstream
 if [[ $SYSTEMD == 1 ]]; then
     echo "Restarting AdGuardHome service..."
 else
