f5364f2297d748eb25efaf4e4c266501e639c3fa
sumuen <zim29144@gmail.com>
2023-11-16 23:49:58 +0800

    fix mistake

--- one_step.sh
+++ one_step.sh
@@ -5,7 +5,7 @@ if [ ! -d "/usr/local/bin" ]; then
     echo "已创建 /usr/local/bin 目录。"
 fi
 # 下载 upstream.sh 脚本并赋予执行权限
-curl -o "/usr/local/bin/upstream.sh" "https://github.com/sumuen/adguardhome-upstream/blob/master/upstream.sh"
+curl -o "/usr/local/bin/upstream.sh" "https://raw.githubusercontent.com/sumuen/adguardhome-upstream/master/upstream.sh"
 chmod +x /usr/local/bin/upstream.sh
 # 运行 upstream.sh 脚本
 /usr/local/bin/upstream.sh
