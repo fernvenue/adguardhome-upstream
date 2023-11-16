a43b955d7ac2270e640652a1e2ced494db789acb
sumuen <zim29144@gmail.com>
2023-11-17 00:12:05 +0800

    update one_step

--- one_step.sh
+++ one_step.sh
@@ -4,6 +4,12 @@ if [ ! -d "/usr/local/bin" ]; then
     mkdir -p /usr/local/bin
     echo "已创建 /usr/local/bin 目录。"
 fi
+if [ -f "/usr/local/bin/upstream.sh.backup" ]; then
+    rm /usr/local/bin/upstream.sh.backup
+fi
+if [ -f "/usr/local/bin/upstream.sh" ]; then
+    mv /usr/local/bin/upstream.sh /usr/local/bin/upstream.sh.backup
+fi
 # 下载 upstream.sh 脚本并赋予执行权限
 curl -o "/usr/local/bin/upstream.sh" "https://raw.githubusercontent.com/sumuen/adguardhome-upstream/master/upstream.sh"
 chmod +x /usr/local/bin/upstream.sh
