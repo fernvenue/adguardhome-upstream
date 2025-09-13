cc32b66dd951fa0740f62926a80894b6460ce157
fernvenue <git@fernvenue.com>
2025-09-14 03:17:09 +0800

    Remove IP version detection.

--- upstream.sh
+++ upstream.sh
@@ -13,34 +13,8 @@ log() {
 }
 set -e
 
-log "IPv4 connection testing..."
-
-if ping -c 3 "223.6.6.6" > /dev/null 2>&1; then
-	IPv4="true"
-fi
-
-log "IPv6 connection testing..."
-if ping -c 3 "2400:3200:baba::1" > /dev/null 2>&1; then
-	IPv6="true"
-fi
-
-if [[ $IPv4 == "true" ]]; then
-	if [[ $IPv6 == "true" ]]; then
-		log "IPv4 and IPv6 connections both available."
-		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
-	else
-		log "Only IPv4 connection available."
-		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
-	fi
-else
-	if [[ $IPv6 == "true" ]]; then
-		log "Only IPv6 connection available."
-		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
-	else
-		log "No available network connection was detected, please try again."
-		exit 1
-	fi
-fi
+log "Downloading upstream configuration..."
+curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
 
 log "Getting data updates..."
 curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/tmp/chinalist.upstream"
@@ -48,11 +22,7 @@ curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh |
 log "Processing data format..."
 cat "/tmp/default.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
 
-if [[ $IPv4 == "true" ]]; then
-	sed -i "s|114.114.114.114|h3://223.5.5.5:443/dns-query h3://223.6.6.6:443/dns-query|g" /usr/share/adguardhome.upstream
-else
-	sed -i "s|114.114.114.114|2400:3200::1 2400:3200:baba::1|g" /usr/share/adguardhome.upstream
-fi
+sed -i "s|114.114.114.114|h3://223.5.5.5:443/dns-query h3://223.6.6.6:443/dns-query|g" /usr/share/adguardhome.upstream
 
 log "Cleaning..."
 rm /tmp/*.upstream
@@ -63,5 +33,3 @@ systemctl restart AdGuardHome
 log "All finished!"
 
 unset IS_SYSTEMD
-unset IPv4
-unset IPv6
